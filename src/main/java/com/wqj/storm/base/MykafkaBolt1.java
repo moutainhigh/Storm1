a2b20ce51d31f103d062e5066c29924878bcf97d
githubwqj <893453172@qq.com>
2018-06-05 14:31:57 +0800

    1

--- src/main/java/com/wqj/storm/base/MykafkaBolt1.java
+++ src/main/java/com/wqj/storm/base/MykafkaBolt1.java
@@ -6,6 +6,9 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.Map;
 
@@ -18,22 +21,40 @@ public class MykafkaBolt1 extends BaseRichBolt {
 
     OutputCollector collector = null;
 
+    private JedisPool jedisPool;
+
     /**
-     *  以下加入redis进行存储,将数据存储在redis中
+     * 以下加入redis进行存储,将数据存储在redis中
      */
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
 
-        this.collector=outputCollector;
+        this.collector = outputCollector;
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
+        jedisPoolConfig.setMaxIdle(5);
+        //控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；
+        //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
+        //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+        jedisPoolConfig.setMaxTotal(1000 * 100);
+        //表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
+        jedisPoolConfig.setMaxWaitMillis(30);
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setTestOnReturn(true);
+        jedisPool = new JedisPool(jedisPoolConfig, "master", 6379, 20, "123456");
     }
 
     public void execute(Tuple tuple) {
-        Object input=  tuple.getValue(0);
+        Object input = tuple.getValue(0);
 
-
+        Jedis jedis = jedisPool.getResource();
+        jedis.select(0);
+        //用完就关闭
+        jedis.close();
         collector.emit(new Values("1"));
     }
 
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
 
     }
+
 }
